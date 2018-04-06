864949f612783bfa302269dd3ddcf1759b525d77
Anton Nikolskiy <angeloffree@yandex.ru>
2018-04-06 19:58:18 +0300

    Volga CTF 2018 - Master

---
+++ VolgaCTF-2018/master/README.m
@@ -0,0 +1,10 @@
+## Volga CTF 2018 - Master
+
+1. Download a * capture.pcap *
+2. Looks like this file corrupt
+3. Open it in any hex editor
+For example, you can use WxHexEditor 
+4. We need to log in as admin, so search string 'admin'
+5. Get a line adminaep7Woo9eef7quiedooPh0oowDgN
+6. aep7Woo9eef7quiedooPh0oowDgN - is a password for admin
+7. Just log in ;)
